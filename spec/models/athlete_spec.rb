74cb3488f41818e3fcb9be948558eae0b975f21a
Daniel Fournier <defourni19@gmail.com>
2024-10-23 18:07:37 -0400

    add validations for athlete's last name

--- spec/models/athlete_spec.rb
+++ spec/models/athlete_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe Athlete, type: :model do
   describe "validations" do
     context "first_name" do
       it "is valid when first_name is present" do
-        athlete = Athlete.new(first_name: "John")
+        athlete = Athlete.new(first_name: "John", last_name: "Doe")
         expect(athlete).to be_valid
       end
       it "is invalid when first_name is not present" do
-        athlete = Athlete.new(first_name: nil)
+        athlete = Athlete.new(first_name: nil, last_name: "Doe")
+        expect(athlete).not_to be_valid
+      end
+    end
+
+    context "last_name" do
+      it "is valid when last_name is present" do
+        athlete = Athlete.new(first_name: "John", last_name: "Doe")
+        expect(athlete).to be_valid
+      end
+      it "is invalid when last_name is not present" do
+        athlete = Athlete.new(first_name: "John", last_name: nil)
         expect(athlete).not_to be_valid
       end
     end
