cb6808525a8fbdfae101dc20bac91b87655a69b0
Daniel Fournier <defourni19@gmail.com>
2024-10-23 16:13:42 -0400

    add athletes table to database with first name, last name and email columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_23_200810) do
+  create_table "athletes", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
