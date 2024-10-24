af61985dd0ea7b5acb685b3a238520d9fb2e3a1e
Daniel Fournier <defourni19@gmail.com>
2024-10-24 09:16:06 -0400

    refactor athlete validation tests to remove redundency

--- spec/models/athlete_spec.rb
+++ spec/models/athlete_spec.rb
@@ -1,38 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Athlete, type: :model do
+  it "is valid with valid params" do
+    valid_params = {
+      first_name: "John",
+      last_name: "Doe",
+      email: "test@email.com"
+    }
+    athlete = Athlete.new(valid_params)
+    expect(athlete).to be_valid
+  end
+
   describe "validations" do
-    context "first_name" do
-      it "is valid when first_name is present" do
-        athlete = Athlete.new(first_name: "John", last_name: "Doe", email: "test@email.com")
-        expect(athlete).to be_valid
-      end
-      it "is invalid when first_name is not present" do
-        athlete = Athlete.new(first_name: nil, last_name: "Doe", email: "test@email.com")
-        expect(athlete).not_to be_valid
-      end
+    it "is invalid when first_name is not present" do
+      athlete = Athlete.new(first_name: nil, last_name: "Doe", email: "test@email.com")
+      expect(athlete).not_to be_valid
     end
 
-    context "last_name" do
-      it "is valid when last_name is present" do
-        athlete = Athlete.new(first_name: "John", last_name: "Doe", email: "test@email.com")
-        expect(athlete).to be_valid
-      end
-      it "is invalid when last_name is not present" do
-        athlete = Athlete.new(first_name: "John", last_name: nil, email: "test@email.com")
-        expect(athlete).not_to be_valid
-      end
+    it "is invalid when last_name is not present" do
+      athlete = Athlete.new(first_name: "John", last_name: nil, email: "test@email.com")
+      expect(athlete).not_to be_valid
     end
 
-    context "email" do
-      it "is valid when email is present" do
-        athlete = Athlete.new(first_name: "John", last_name: "Doe", email: "test@email.com")
-        expect(athlete).to be_valid
-      end
-      it "is invalid when email is not present" do
-        athlete = Athlete.new(first_name: "John", last_name: "Doe", email: nil)
-        expect(athlete).not_to be_valid
-      end
+    it "is invalid when email is not present" do
+      athlete = Athlete.new(first_name: "John", last_name: "Doe", email: nil)
+      expect(athlete).not_to be_valid
     end
   end
 end
