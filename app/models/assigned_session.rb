3a50ccc552dc156631fe12a6a8f3f44d19c2855e
Daniel Fournier <defourni19@gmail.com>
2024-11-01 14:45:32 -0400

    Add assigned session active record validations and tests

--- app/models/assigned_session.rb
+++ app/models/assigned_session.rb
@@ -1,3 +1,15 @@
 class AssignedSession < ApplicationRecord
   belongs_to :athlete
+
+  validates :date, presence: true
+  validates :details, presence: true
+
+  enum :session_type, {
+    easy_run: 0,
+    workout: 1,
+    long_run: 2,
+    rest_day: 3,
+    cross_training: 4,
+    strength: 5
+  }
 end
