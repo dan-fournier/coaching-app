8ff1e3ff551c1fde52b3ecc66ecc2c8c0db78105
Daniel Fournier <defourni19@gmail.com>
2024-11-03 13:04:50 -0500

    Add WeeklyPlans create action to handle new weekly plan form submissions by creating 7 new assigned sessions for an athlete

--- app/controllers/weekly_plans_controller.rb
+++ app/controllers/weekly_plans_controller.rb
@@ -2,4 +2,53 @@ class WeeklyPlansController < ApplicationController
   def new
     @athlete = Athlete.find(params[:athlete_id])
   end
+
+  def create
+    @athlete = Athlete.find(params[:athlete_id])
+
+    start_date = params[:athlete][:start_date]
+
+    if start_date.present?
+
+      date = Date.parse(start_date)
+
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_0],
+        date: date,
+        details: params[:athlete][:details_0]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_1],
+        date: date + 1,
+        details: params[:athlete][:details_1]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_2],
+        date: date + 2,
+        details: params[:athlete][:details_2]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_3],
+        date: date + 3,
+        details: params[:athlete][:details_3]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_4],
+        date: date + 4,
+        details: params[:athlete][:details_4]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_5],
+        date: date + 5,
+        details: params[:athlete][:details_5]
+      )
+      @athlete.assigned_sessions.create!(
+        session_type: params[:athlete][:session_type_6],
+        date: date + 6,
+        details: params[:athlete][:details_6]
+      )
+    end
+
+    redirect_to athlete_path(@athlete), notice: "New plan successfully created"
+  end
 end
--- spec/requests/weekly_plans_spec.rb
+++ spec/requests/weekly_plans_spec.rb
@@ -10,4 +10,66 @@ RSpec.describe "WeeklyPlans", type: :request do
       expect(response.body).to include("Add Weekly Plan")
     end
   end
+
+  describe "POST /athletes/:athlete_id/weekly_plans" do
+    context "with valid params" do
+      valid_params = { athlete: {
+        start_date: "2024-10-28",
+        session_type_0: "easy_run",
+        details_0: "testing",
+        session_type_1: "easy_run",
+        details_1: "testing",
+        session_type_2: "easy_run",
+        details_2: "testing",
+        session_type_3: "easy_run",
+        details_3: "testing",
+        session_type_4: "easy_run",
+        details_4: "testing",
+        session_type_5: "easy_run",
+        details_5: "testing",
+        session_type_6: "easy_run",
+        details_6: "testing"
+      } }
+      it "creates seven new assigned sessions for an athlete" do
+        expect {
+          post athlete_weekly_plans_path(athlete), params: valid_params
+      }.to change(AssignedSession, :count).by(7)
+      end
+
+      it "redirects to the athlete show page" do
+        post athlete_weekly_plans_path(athlete), params: valid_params
+
+        expect(response).to redirect_to athlete_path(athlete)
+
+        follow_redirect!
+
+        expect(response.body).to include("New plan successfully created")
+      end
+    end
+    context "with no start_date" do
+      invalid_params = { athlete: {
+        start_date: "",
+        session_type_0: "easy_run",
+        details_0: "testing",
+        session_type_1: "easy_run",
+        details_1: "testing",
+        session_type_2: "easy_run",
+        details_2: "testing",
+        session_type_3: "easy_run",
+        details_3: "testing",
+        session_type_4: "easy_run",
+        details_4: "testing",
+        session_type_5: "easy_run",
+        details_5: "testing",
+        session_type_6: "easy_run",
+        details_6: "testing"
+      } }
+
+      it "does not create any new assigned sessions for an athlete" do
+        expect {
+          post athlete_weekly_plans_path(athlete), params: invalid_params
+      }.to change(AssignedSession, :count).by(0)
+      end
+    end
+  end
 end
